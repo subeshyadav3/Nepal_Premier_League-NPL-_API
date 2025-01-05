04bbc234ec5c2ecac3b2e1c2181a4602792552a9
Subesh Yadav <41501661+subeshyadav3@users.noreply.github.com>
2025-01-05 20:58:00 +0545

    update.1

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
+const path = require('path');
 const app = express();
 dotenv.config();
 const apiRoutes = require('./routes/api');
 
+
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.use('/api', apiRoutes);
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index');  
 });
+
 
 // Start the server
 const port = process.env.PORT || 8000;
